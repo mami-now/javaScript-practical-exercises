df0fe5613a03ef492d772c3d24e95deaeb63e8c8
mamijose <mmammyj@gmail.com>
2023-07-19 12:03:23 +0200

    2nd commit

--- countingZeroes.js
+++ countingZeroes.js
@@ -1,5 +1,11 @@
+// n= 5 ,(1-50)
+// n= 10,(1-100)
+// n= ? ,(1-n)
+// value=100,200,10000
+
 let myArray=[];
-for(i=0;i<=50;i++){
+for(i=0;i<=100;i++){
   myArray.push(i);
 }
-console.log(myArray)
+console.log(myArray);
+myArray.find

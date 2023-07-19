41d074bc096786672e85e133266bc3dff9fa4a28
mamijose <mmammyj@gmail.com>
2023-07-19 15:21:47 +0200

    the 3rd question

--- greatestcommonDivisor.js
+++ greatestcommonDivisor.js
@@ -1,9 +1,13 @@
-let a=10;
-let b=20;
+
 let divisor;
 
 function greatestCd(a,b){
   for(let i=1;i<=a && i<=b ;i++ ){
-divisor=i;
+
+  if(a % i ==0 && b % i==0 ){
+    divisor=i;
   }
 }
+ return divisor;
+}
+console.log(greatestCd(10,20))
---
+++ remove duplicates.js
@@ -0,0 +1,12 @@
+
+let arr=[mangoes,pineaples,banana,mangoes];
+let newArr=[];
+
+  for(let i=0;i<arr.lenght ;i++){
+   if(!newArr.includes(arr[i]))
+    newArr.push(arr[i])
+  }
+
+    
+    
+  console.log(newArr)
